80870843c2c48c7140538f80de41c0bdbfc28c75
panikwonthelp <waltrick.thedev@gmail.com>
2023-02-22 09:44:36 +0700

    abc

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0x55ACed79AeDA77F0F73B76f58ed96EEe3a507D51";
+export const tokenContractAddress =
+  "0x7D6d62D0FF0D42E2E1C2AA9991594493cdAf5D64";
+export const stakingContractAddress =
+  "0x463a323764b74B0FCe9FAe786e67E6BfA9e4962D";
