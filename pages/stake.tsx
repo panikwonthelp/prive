9204155670477668553d55d88703a207d43c135a
panikwonthelp <waltrick.thedev@gmail.com>
2023-02-26 08:39:38 +0700

    multiples stake and unstake

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -34,7 +34,9 @@ const Stake: NextPage = () => {
   const { data: tokenBalance } = useTokenBalance(tokenContract, address);
   const [claimableRewards, setClaimableRewards] = useState<BigNumber>();
   const [inputValue, setInputValue] = useState<string>("");
-  const { data: stakedTokens } = useContractRead(
+  const [withdrawInputValue, setWithdrawInputValue] = useState<string>("");
+
+  const { data: stakedTokens, refetch: refetchStakedTokens } = useContractRead(
     contract,
     "getStakeInfo",
     address
@@ -51,97 +53,113 @@ const Stake: NextPage = () => {
     loadClaimableRewards();
   }, [address, contract]);
 
-  async function stakeNft(ids: string[]) {
-    if (!address) return;
-  
-    const isApproved = await nftDropContract?.isApproved(
-      address,
-      stakingContractAddress
-    );
-    if (!isApproved) {
-      await nftDropContract?.setApprovalForAll(stakingContractAddress, true);
-    }
-  
-    try {
-      for (const id of ids) {
-        await contract?.call("stake", [id]);
-      }
-    } catch (error: any) {
-      if (error.name === "TransactionRevertedWithoutReasonError") {
-        alert("Transaction cancelled");
-      } else {
-        console.log(error);
-      }
-    }
-  }
-  if (isLoading) {
-    return <div>Loading</div>;
-  }
+  async function stakeNft(ids: number[]): Promise<void> {
+  if (!address) return;
 
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.h1}>Stake Your NFTs</h1>
-      <input
-  type="text"
-  placeholder="Enter NFT ID(s) separated by commas"
-  value={inputValue}
-  onChange={(e) => setInputValue(e.target.value)}
-/>
-      <button onClick={() => stakeNft(inputValue.split(","))}>Stake NFT(s)</button>
-      <text className="text1">Enter NFT IDs separated by commas. Staking multiples NFTs at once will trigger multiples transactions.</text>
-      <hr className={`${styles.divider} ${styles.spacerTop}`} />
-
-      {!address ? (
-        <ConnectWallet />
-      ) : (
-        <>
-          <h2>Your Tokens</h2>
-          <div className={styles.tokenGrid}>
-            <div className={styles.tokenItem}>
-              <h3 className={styles.tokenLabel}>Claimable Rewards</h3>
-              <p className={styles.tokenValue}>
-                <b>
-                  {!claimableRewards
-                    ? "Loading..."
-                    : ethers.utils.formatUnits(claimableRewards, 18)}
-                </b>{" "}
-                {tokenBalance?.symbol}
-              </p>
-            </div>
-            <div className={styles.tokenItem}>
-              <h3 className={styles.tokenLabel}>Current Balance</h3>
-              <p className={styles.tokenValue}>
-                <b>{tokenBalance?.displayValue}</b> {tokenBalance?.symbol}
-              </p>
-            </div>
-          </div>
-
-          <Web3Button
-            action={(contract) => contract.call("claimRewards")}
-            contractAddress={stakingContractAddress}
-          >
-            Claim Rewards
-          </Web3Button>
-
-          <hr className={`${styles.divider} ${styles.spacerTop}`} />
-          <h2>Your Staked NFTs</h2>
-          <div className={styles.nftBoxGrid}>
-            {stakedTokens &&
-              stakedTokens[0]?.map((stakedToken: BigNumber) => (
-                <NFTCard
-                  tokenId={stakedToken.toNumber()}
-                  key={stakedToken.toString()}
-                />
-              ))}
-          </div>
-
-          <hr className={`${styles.divider} ${styles.spacerTop}`} />
-          <h2>Your Unstaked NFTs</h2>
-          
-        </>
-      )}
-    </div>
+  const isApproved = await nftDropContract?.isApproved(
+    address,
+    stakingContractAddress
   );
+  if (!isApproved) {
+    await nftDropContract?.setApprovalForAll(stakingContractAddress, true);
+  }
+
+  try {
+    await contract?.call("stake", ids);
+    refetchStakedTokens();
+  } catch (error: any) {
+    if (error.name === "TransactionRevertedWithoutReasonError") {
+      alert("Transaction cancelled");
+    } else {
+      console.log(error);
+    }
+  }
+}
+  async function withdrawNft(nftIds: number[]): Promise<void> {
+    if (!address) return;
+try {
+await contract?.call("withdraw", nftIds);
+} catch (error: any) {
+if (error.name === "TransactionRevertedWithoutReasonError") {
+alert("Transaction cancelled");
+} else {
+console.log(error);
+}
+}
+}
+
+return (
+<div className={styles.container}>
+<h1 className={styles.h1}>Stake Your NFTs</h1>
+<input
+type="text"
+placeholder="Enter NFT ID(s) separated by commas"
+value={inputValue}
+onChange={(e) => setInputValue(e.target.value)}
+/>
+<button onClick={() => stakeNft(inputValue.split(",").map(Number))}>Stake NFT(s)</button>
+<text className="text1">Enter NFT IDs separated by commas. Staking multiples NFTs at once will trigger multiples transactions.</text>
+<hr className={`${styles.divider} ${styles.spacerTop}`} />
+{!address ? (
+    <ConnectWallet />
+  ) : (
+    <>
+      <h2>Your Tokens</h2>
+      <div className={styles.tokenGrid}>
+        <div className={styles.tokenItem}>
+          <h3 className={styles.tokenLabel}>Claimable Rewards</h3>
+          <p className={styles.tokenValue}>
+            <b>
+              {!claimableRewards
+                ? "Loading..."
+                : ethers.utils.formatUnits(claimableRewards, 18)}
+            </b>{" "}
+            {tokenBalance?.symbol}
+          </p>
+        </div>
+        <div className={styles.tokenItem}>
+          <h3 className={styles.tokenLabel}>Current Balance</h3>
+          <p className={styles.tokenValue}>
+            <b>{tokenBalance?.displayValue}</b> {tokenBalance?.symbol}
+          </p>
+        </div>
+      </div>
+
+      <Web3Button
+        action={(contract) => contract.call("claimRewards")}
+        contractAddress={stakingContractAddress}
+      >
+        Claim Rewards
+      </Web3Button>
+
+      <hr className={`${styles.divider} ${styles.spacerTop}`} />
+      <h2>Your Staked NFTs</h2>
+      <div className={styles.nftBoxGrid}>
+        {stakedTokens &&
+          stakedTokens[0]?.map((stakedToken: BigNumber) => (
+            <NFTCard
+              tokenId={stakedToken.toNumber()}
+              key={stakedToken.toString()}
+            />
+          ))}
+      </div>
+
+      <hr className={`${styles.divider} ${styles.spacerTop}`} />
+      <h2>Unstake multiples NFTs</h2>
+      <input
+        type="text"
+        placeholder="Enter NFT ID(s) separated by commas"
+        value={withdrawInputValue}
+        onChange={(e) => setWithdrawInputValue(e.target.value)}
+      />
+      <button onClick={() => withdrawNft(withdrawInputValue.split(",").map(Number))}>Withdraw NFT(s)</button>
+
+      <text className="text1">Enter NFT IDs separated by commas. Withdrawing multiples NFTs at once will trigger only one transaction.</text>
+      <hr className={`${styles.divider} ${styles.spacerTop}`} />
+    </>
+  )}
+</div>
+);
 };
 
 export default Stake;
